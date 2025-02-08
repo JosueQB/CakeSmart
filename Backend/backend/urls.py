8c3c93cf9ff47847f0fb64c4d34d1ce2be849e9c
Josue Quijije <85885543+JosueQB@users.noreply.github.com>
2025-02-07 21:29:48 -0500

    Cambios de backend
    Arreglo de Backend con Josue Quijije

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 from ingredients.viewset import IngredientsViewSet
 from product.viewset import ProductIngredientsViewSet, ProductViewSet
@@ -39,3 +41,5 @@ router.register(r'ingredients', IngredientsViewSet, basename='ingredients')
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
